eb0f164c325e9f356b807b91cacc3ae6e1d3ec89
lenndachen <49460507+lenndachen@users.noreply.github.com>
2019-08-30 16:16:45 -0400

    Initial commit

---
+++ src/constants/routes.js
@@ -0,0 +1,14 @@
+export const ACCOUNT = "/account"
+export const ADMIN = "/admin"
+export const APP = '/app'
+export const FIREBASE = '/firebase'
+export const HOME ='/home'
+export const LANDING ='/landing'
+export const NAVIGATION ='/navigation' 
+export const PASSWORDCHANGE ='/passwordchange'
+export const PASSWORDFORGET ='/passwordforget'
+export const SESSION ='/session'
+export const SIGNIN ='/signin'
+export const SIGNOUT ='/signout'
+export const SIGNUP ='/signup'
+
