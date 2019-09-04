852e43ce733d45d6bd31f69e40e59f1863c7bcdf
lenndachen <49460507+lenndachen@users.noreply.github.com>
2019-09-04 10:12:45 -0400

    10:12 9/4/2019

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,37 +1,34 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
+
 import { withFirebase } from '../Firebase';
-import { FirebaseContext } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
-import { compose } from 'recompose';
 
 const SignUpPage = () => (
   <div>
     <h1>SignUp</h1>
     <SignUpForm />
-    <FirebaseContext.Consumer>
-      {firebase => <SignUpForm firebase={firebase} />}
-    </FirebaseContext.Consumer>
-    <SignUpForm />
   </div>
 );
 
 const INITIAL_STATE = {
-    username: '',
-    email: '',
-    passwordOne: '',
-    passwordTwo: '',
-    error: null,
-  };
+  username: '',
+  email: '',
+  passwordOne: '',
+  passwordTwo: '',
+  error: null,
+};
 
 class SignUpFormBase extends Component {
-
   constructor(props) {
     super(props);
+
     this.state = { ...INITIAL_STATE };
   }
+
   onSubmit = event => {
     const { username, email, passwordOne } = this.state;
+
     this.props.firebase
       .doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
@@ -41,30 +38,32 @@ class SignUpFormBase extends Component {
       .catch(error => {
         this.setState({ error });
       });
+
     event.preventDefault();
-  }
+  };
+
   onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
   render() {
     const {
-        username,
-        email,
-        passwordOne,
-        passwordTwo,
-        error,
-      } = this.state;
+      username,
+      email,
+      passwordOne,
+      passwordTwo,
+      error,
+    } = this.state;
 
-      const isInvalid =
+    const isInvalid =
       passwordOne !== passwordTwo ||
       passwordOne === '' ||
       email === '' ||
       username === '';
-      
+
     return (
       <form onSubmit={this.onSubmit}>
-       <input
+        <input
           name="username"
           value={username}
           onChange={this.onChange}
@@ -92,7 +91,7 @@ class SignUpFormBase extends Component {
           type="password"
           placeholder="Confirm Password"
         />
-         <button disabled={isInvalid} type="submit">
+        <button disabled={isInvalid} type="submit">
           Sign Up
         </button>
 
@@ -101,14 +100,14 @@ class SignUpFormBase extends Component {
     );
   }
 }
+
 const SignUpLink = () => (
   <p>
     Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
   </p>
 );
 
-const SignUpForm = compose(withRouter,withFirebase,)(SignUpFormBase);
-
+const SignUpForm = withRouter(withFirebase(SignUpFormBase));
 
 export default SignUpPage;
 export { SignUpForm, SignUpLink };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import Firebase, { FirebaseContext } from './components/Firebase';
+import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase/index.js';
 
 ReactDOM.render(
-    <FirebaseContext.Provider value={new Firebase()} />,<App />,<FirebaseContext.Provider />,document.getElementById('root'));
-  serviceWorker.unregister();
-
-  
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById('root'),
+);
+serviceWorker.unregister();
