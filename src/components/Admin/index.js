bf903edb9f3657420ea7273ed8c7c7a901e9bd40
lenndachen <49460507+lenndachen@users.noreply.github.com>
2019-09-11 16:08:26 -0400

    9.11.19 4:08 Worked on Part 2 of Firebase Project

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
+import { compose } from 'recompose';
+
 import { withFirebase } from '../Firebase';
+import { withAuthorization } from '../Session';
+import * as ROLES from '../../constants/roles';
+
 class AdminPage extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       loading: false,
       users: [],
     };
   }
+
   componentDidMount() {
     this.setState({ loading: true });
+
     this.props.firebase.users().on('value', snapshot => {
-        const usersObject = snapshot.val();
-        const usersList = Object.keys(usersObject).map(key => ({
-            ...usersObject[key],
-            uid: key,
-          }));
+      const usersObject = snapshot.val();
+
+      const usersList = Object.keys(usersObject).map(key => ({
+        ...usersObject[key],
+        uid: key,
+      }));
+
       this.setState({
         users: usersList,
         loading: false,
@@ -27,27 +37,34 @@ class AdminPage extends Component {
     this.props.firebase.users().off();
   }
 
- render() {
+  render() {
     const { users, loading } = this.state;
+
     return (
       <div>
         <h1>Admin</h1>
+        <p>
+          The Admin Page is accessible by every signed in admin user.
+        </p>
+
         {loading && <div>Loading ...</div>}
+
         <UserList users={users} />
       </div>
     );
   }
 }
-   const UserList = ({ users }) => (
-    <ul>
-            {users.map(user => (
+
+const UserList = ({ users }) => (
+  <ul>
+    {users.map(user => (
       <li key={user.uid}>
-         <span>
+        <span>
           <strong>ID:</strong> {user.uid}
-         </span>
-         <span>
+        </span>
+        <span>
           <strong>E-Mail:</strong> {user.email}
-         </span>
+        </span>
         <span>
           <strong>Username:</strong> {user.username}
         </span>
@@ -55,4 +72,11 @@ class AdminPage extends Component {
     ))}
   </ul>
 );
-export default withFirebase(AdminPage);;
+
+const condition = authUser =>
+  authUser && authUser.roles.includes(ROLES.ADMIN);
+
+export default compose(
+  withAuthorization(condition),
+  withFirebase,
+)(AdminPage);
